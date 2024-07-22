94f59a3db0df43f4964da3001fc95666e908b85a
MoonInbae <mib010213@gmail.com>
2024-07-22 15:34:06 +0900

    [Chore] delete annotation SpringBootTest

--- src/test/java/org/ohmystomach/ohmystomach_server/OhMyStomachServerApplicationTests.java
+++ src/test/java/org/ohmystomach/ohmystomach_server/OhMyStomachServerApplicationTests.java
@@ -3,7 +3,7 @@ package org.ohmystomach.ohmystomach_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class OhMyStomachServerApplicationTests {
 
     @Test
