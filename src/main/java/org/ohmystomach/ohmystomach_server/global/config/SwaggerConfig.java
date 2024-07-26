1995481fa8c79697ba42ae55e6c1f4471affbedc
MoonInbae <mib010213@gmail.com>
2024-07-27 00:20:17 +0900

    [Chore] Initial commit

---
+++ src/main/java/org/ohmystomach/ohmystomach_server/global/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package org.ohmystomach.ohmystomach_server.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("흡변 API")
+                        .version("v1.0.0").
+                        description("아이고배야 팀에서 만든 흡변 서비스 API입니다.")
+                );
+    }
+}
