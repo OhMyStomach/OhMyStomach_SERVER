d445854c15498cc81498503efb9b2365bf2e55c7
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-08-05 23:54:32 +0900

    [Feat] api 설명 추가

--- src/main/java/org/ohmystomach/ohmystomach_server/domain/smokemyplace/api/UserSmokeController.java
+++ src/main/java/org/ohmystomach/ohmystomach_server/domain/smokemyplace/api/UserSmokeController.java
@@ -1,22 +1,22 @@
 package org.ohmystomach.ohmystomach_server.domain.smokemyplace.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.ohmystomach.ohmystomach_server.domain.oauth.application.JWTService;
 import org.ohmystomach.ohmystomach_server.domain.smokemyplace.application.UserSmokeService;
+import org.ohmystomach.ohmystomach_server.domain.smokemyplace.domain.UserSmoke;
 import org.ohmystomach.ohmystomach_server.domain.smokemyplace.dto.request.CreateUserSmokeRequestDto;
 import org.ohmystomach.ohmystomach_server.domain.smokemyplace.dto.request.UpdateUserSmokeRequestDto;
-import org.ohmystomach.ohmystomach_server.domain.toiletmyplace.dto.request.CreateUserToiletRequestDto;
-import org.ohmystomach.ohmystomach_server.domain.toiletmyplace.dto.request.UpdateUserToiletRequestDto;
-import org.springframework.web.bind.annotation.*;
-import org.ohmystomach.ohmystomach_server.domain.smoke.domain.Smoke;
-import org.ohmystomach.ohmystomach_server.domain.smokemyplace.domain.UserSmoke;
 import org.ohmystomach.ohmystomach_server.global.common.response.ApiResponse;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
  * 사용자가 저장한 흡연구역 데이터와 관련된 요청을 처리하는 컨트롤러.
  */
+@Tag(name = "내 장소 - 흡연구역 API", description = "내 흡연구역 생성, 조회, 수정, 삭제")
 @RestController
 @RequestMapping("/api/my/smoke")
 @RequiredArgsConstructor
@@ -31,6 +31,7 @@ public class UserSmokeController {
      * @param userId 사용자의 ID.
      * @return 사용자가 저장한 흡연구역 목록을 포함하는 ApiResponse.
      */
+    @Operation(summary = "내 흡연구역 정보 조회 api", description = "사용자가 내 장소로 저장한 흡연구역 목록을 조회합니다.")
     @GetMapping("/all")
     public ApiResponse<List<UserSmoke>> retrieveUserSavedSmokes(@RequestHeader("Authorization") String token) {
         return userSmokeService.retrieveUserSavedSmokes(jwtService.decodeToken(token));
@@ -43,6 +44,7 @@ public class UserSmokeController {
      * @param smoke 요청 본문으로 전달된 흡연구역 객체.
      * @return 저장된 UserSmoke 객체를 포함하는 ApiResponse.
      */
+    @Operation(summary = "내 흡연구역 정보 생성 api", description = "새로운 흡연구역을 사용자의 내 장소로 저장합니다.")
     @PostMapping("/save")
     public ApiResponse<UserSmoke> createUserSmoke(@RequestBody CreateUserSmokeRequestDto dto) {
         return userSmokeService.createUserSmoke(dto.toServiceDto(jwtService.decodeToken(dto.token())));
@@ -55,8 +57,10 @@ public class UserSmokeController {
      * @param smokeId 삭제할 Smoke의 ID.
      * @return 삭제 결과 메시지를 포함하는 ApiResponse.
      */
+    @Operation(summary = "내 흡연구역 정보 삭제 api", description = "사용자가 내 장소로 저장한 흡연구역을 삭제합니다.")
     @DeleteMapping("/{smokeId}")
-    public ApiResponse<String> deleteUserSmoke(@RequestHeader("Authorization") String token, @PathVariable Long smokeId) {
+    public ApiResponse<String> deleteUserSmoke(@RequestHeader("Authorization") String token,
+                                               @PathVariable Long smokeId) {
         return userSmokeService.deleteUserSmoke(jwtService.decodeToken(token), smokeId);
     }
 
@@ -68,6 +72,7 @@ public class UserSmokeController {
      * @param updatedSmoke 업데이트할 흡연구역의 새로운 정보.
      * @return 업데이트된 UserSmoke 객체를 포함하는 ApiResponse.
      */
+    @Operation(summary = "내 흡연구역 정보 수정 api", description = "사용자가 내 장소로 저장한 흡연구역의 정보를 업데이트합니다.")
     @PutMapping("edit")
     public ApiResponse<UserSmoke> updateUserSmoke(@RequestBody UpdateUserSmokeRequestDto dto) {
         return userSmokeService.updateUserSmoke(dto.toServiceDto(jwtService.decodeToken(dto.token())));
--- src/main/java/org/ohmystomach/ohmystomach_server/domain/toiletmyplace/api/UserToiletController.java
+++ src/main/java/org/ohmystomach/ohmystomach_server/domain/toiletmyplace/api/UserToiletController.java
@@ -1,5 +1,7 @@
 package org.ohmystomach.ohmystomach_server.domain.toiletmyplace.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.ohmystomach.ohmystomach_server.domain.oauth.application.JWTService;
 import org.ohmystomach.ohmystomach_server.domain.toiletmyplace.application.UserToiletService;
@@ -14,6 +16,7 @@ import java.util.List;
 /**
  * 사용자가 저장한 화장실 데이터와 관련된 요청을 처리하는 컨트롤러.
  */
+@Tag(name = "내 장소 - 화장실 API", description = "내 화장실 생성, 조회, 수정, 삭제")
 @RestController
 @RequestMapping("/api/my/toilet")
 @RequiredArgsConstructor
@@ -28,6 +31,7 @@ public class UserToiletController {
      * @param token 사용자의 ID.
      * @return 사용자가 저장한 화장실 목록을 포함하는 ApiResponse.
      */
+    @Operation(summary = "내 화장실 정보 조회 api", description = "사용자가 내 장소로 저장한 화장실 목록을 조회합니다.")
     @GetMapping("/all")
     public ApiResponse<List<UserToilet>> retrieveUserSavedToilets(@RequestHeader("Authorization") String token) {
         return userToiletService.retrieveUserSavedToilets(jwtService.decodeToken(token));
@@ -40,6 +44,7 @@ public class UserToiletController {
      * @param dto 요청 본문으로 전달된 화장실 객체.
      * @return 저장된 UserToilet 객체를 포함하는 ApiResponse.
      */
+    @Operation(summary = "내 화장실 정보 생성 api", description = "새로운 화장실을 사용자의 내 장소로 저장합니다.")
     @PostMapping("/save")
     public ApiResponse<UserToilet> createUserToilet(@RequestBody CreateUserToiletRequestDto dto) {
         return userToiletService.createUserToilet(dto.toServiceDto(jwtService.decodeToken(dto.token())));
@@ -52,8 +57,10 @@ public class UserToiletController {
      * @param toiletId 삭제할 화장실의 ID.
      * @return 삭제 결과 메시지를 포함하는 ApiResponse.
      */
+    @Operation(summary = "내 화장실 정보 삭제 api", description = "사용자가 내 장소로 저장한 화장실을 삭제합니다.")
     @DeleteMapping("/{toiletId}")
-    public ApiResponse<String> deleteUserToilet(@RequestHeader("Authorization") String token, @PathVariable Long toiletId) {
+    public ApiResponse<String> deleteUserToilet(@RequestHeader("Authorization") String token,
+                                                @PathVariable Long toiletId) {
         return userToiletService.deleteUserToilet(jwtService.decodeToken(token), toiletId);
     }
 
@@ -65,6 +72,7 @@ public class UserToiletController {
      * @param dto 업데이트할 화장실의 새로운 정보.
      * @return 업데이트된 UserToilet 객체를 포함하는 ApiResponse.
      */
+    @Operation(summary = "내 화장실 정보 수정 api", description = "사용자가 내 장소로 저장한 화장실의 정보를 업데이트합니다.")
     @PutMapping("edit")
     public ApiResponse<UserToilet> updateUserToilet(@RequestBody UpdateUserToiletRequestDto dto) {
         return userToiletService.updateUserToilet(dto.toServiceDto(jwtService.decodeToken(dto.token())));
