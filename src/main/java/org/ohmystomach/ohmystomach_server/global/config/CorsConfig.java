c49f9ed86c409c2ae42731b4eb7080faaadf9f7c
MoonInbae <mib010213@gmail.com>
2024-08-30 23:19:12 +0900

    [Feat]CORS 설정 변경

--- src/main/java/org/ohmystomach/ohmystomach_server/global/config/CorsConfig.java
+++ src/main/java/org/ohmystomach/ohmystomach_server/global/config/CorsConfig.java
@@ -10,8 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//			.allowedOrigins("*")
-                .allowedOriginPatterns("*") // 쿠키를 받기 위해 기존코드(allowedOrigin) -> 새로운 코드(allowedOriginPatterns)
+			    .allowedOrigins("*")
+//                .allowedOriginPatterns("*") // 쿠키를 받기 위해 기존코드(allowedOrigin) -> 새로운 코드(allowedOriginPatterns)
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true) // 자격 증명 허용
